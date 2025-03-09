c49170607ae5db0cde7aa9001802465937aa4097
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-03-10 08:10:13 +0900

    Create new migration file to update category table and create new pivot table category_post, then add BelongsTomany relatonship to Post and Category Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
@@ -14,5 +16,10 @@ class Post extends Model
 
     public function user() {
         return $this->belongsTo(User::class); 
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
