2b6c56274cb9e27fc87d9ad2fb5c8a327162f135
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-03-11 23:42:21 +0900

    Update style for read more button: push to to bottom of the post entry card

--- resources/views/components/post-readmore.blade.php
+++ resources/views/components/post-readmore.blade.php
@@ -1,7 +1,7 @@
 @props(['post'])
-<div class="mt-4">
+<div class="mt-auto">
   <a href="{{ route('post.show', $post->slug) }}" 
-  class="inline-block border-2 border-solid border-blue-600  px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-white">
+  class="inline-block border-2 border-solid text-blue-600 border-blue-600  px-4 py-2 rounded-lg hover:bg-blue-600 hover:text-white">
   Read More →
   </a>
 </div>
