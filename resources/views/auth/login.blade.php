2eed54acc08ff4974b8addde56969cbcdff88ee2
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-03-12 23:44:20 +0900

    fix login form error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,18 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
+       {{--  <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
+ --}}
+        
+            <x-form-input name="email" label="Email" prequired />
+            <x-form-input type="password" name="password" label="Password" required />
+         
+       
+      {{-- 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
@@ -23,7 +29,7 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
+ --}}
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
